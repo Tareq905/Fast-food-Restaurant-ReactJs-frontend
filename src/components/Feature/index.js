82936a83a2c0d0c417f7185f88bf84cd04e56d77
Tareq905 <tareqshah.027@gmail.com>
2022-12-31 05:25:33 +0600

    first commit

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Best Food, best quality</h1>
+      <p>Eat healthy & Stay healthy!</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn
+} from './HeroElements';
+
+const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <HeroContainer>
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <HeroContent>
+        <HeroItems>
+          <HeroH1>Best Food, Best quality</HeroH1>
+          <HeroP>Ready in 5 minutes</HeroP>
+          <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
